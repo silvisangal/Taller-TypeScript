33617b111a2440747642db52a50882b793419178
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2023-04-16 18:46:41 -0500

    Actualización

---
+++ Serie.js
@@ -0,0 +1,12 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, estudio, rating, description, url, image) {
+        this.id = id;
+        this.name = name;
+        this.estudio = estudio;
+        this.rating = rating;
+        this.description = description;
+        this.url = url;
+        this.image = image;
+    }
+    return Serie;
+}());
---
+++ Serie.ts
@@ -0,0 +1,6 @@
+class Serie{
+    constructor(private id: number, private name: string, private estudio: string, private rating: number, private description: string, private url: string, private image: string){
+    }
+
+    
+}
