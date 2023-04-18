08bced017096e6c67046896a8c87e0a3c9e7d2d6
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2023-04-17 21:10:51 -0500

    Proyectp terminado

--- main.ts
+++ main.ts
@@ -1,17 +1,56 @@
-import { Serie } from "./Serie";
-import { series } from "./data";
+import { Serie } from "./Serie.js";
+import { series } from "./data.js";
 
 const seriesTbody: HTMLElement = document.getElementById('coleccion')!; 
 
+renderSeriesInTable(series);
+
 function renderSeriesInTable(series: Serie[]): void {
-  series.forEach (c  => {
+  series.forEach ((serie)  => {
     let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
-                           <td>${c.estudio}</td>
-                           <td>${c.rating}</td>`;
+    trElement.innerHTML = `<td>${serie.id}</td>
+                           <td><a herf = "#" class = "clickable" data-id = "${serie.name}">${serie.name}</a></td>
+                           <td>${serie.estudio}</td>
+                           <td>${serie.rating}</td>`;
     seriesTbody.appendChild(trElement);
   });
 }
 
-renderSeriesInTable(series);
+function renderAverage(average: number): void {
+  const averageElement: HTMLElement = document.getElementById('average')!;
+  let prom: string = average.toString();
+  averageElement.innerHTML += prom;
+
+}
+
+function getAverageSeasons(series: Serie[]): void {
+  //Rating en este caso, por error de programación, sería igual a temporada
+  let totalCredits: number = 0;
+  series.forEach((serie) => totalCredits = totalCredits + serie.rating);
+  let promedio : number = totalCredits / series.length;
+  renderAverage(promedio);
+}
+
+function searchSerieByName(nameKey: string, series: Serie[]) {
+  const matchingSeries = nameKey === '' ? series : series.filter( serie => serie.name.match(nameKey));
+  return matchingSeries[0]
+}
+
+//El siguiente método fue obtenido del trabajo de Diego Ortiz, las pequeñas variantes del tamaño
+//de la imagen fueron realizadas por mi al igual que la distribución de los elementos en la card
+document.addEventListener("click", (event: MouseEvent) => {
+  const triggered = event.target as HTMLElement;
+  if (triggered.classList.contains("clickable")) {
+    const nombre = triggered.dataset.id as string;
+    let serieInfo : Serie = searchSerieByName(nombre, series);
+    const cardTitle = document.querySelector('#cardTitle') as HTMLElement;
+    const cardText = document.querySelector('#cardText') as HTMLElement;
+    cardTitle.textContent = serieInfo.name;
+    cardText.innerHTML = `
+      <p><img class="serie-image" src="${serieInfo.image}" alt="${serieInfo.name} image" width = "250" height ="150" ></p>
+      <p>${serieInfo.description}</p>
+      <p><a href="${serieInfo.url}" target="_blank">${serieInfo.url}</a></p>
+      
+    `;
+  }
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,39 @@
-import { series } from "./data";
+import { series } from "./data.js";
 var seriesTbody = document.getElementById('coleccion');
+renderSeriesInTable(series);
 function renderSeriesInTable(series) {
-    series.forEach(function (c) {
+    series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                           <td>").concat(c.name, "</td>\n                           <td>").concat(c.estudio, "</td>\n                           <td>").concat(c.rating, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><a herf = \"#\" class = \"clickable\" data-id = \"").concat(serie.name, "\">").concat(serie.name, "</a></td>\n                           <td>").concat(serie.estudio, "</td>\n                           <td>").concat(serie.rating, "</td>");
         seriesTbody.appendChild(trElement);
     });
 }
-renderSeriesInTable(series);
+function renderAverage(average) {
+    var averageElement = document.getElementById('average');
+    var prom = average.toString();
+    averageElement.innerHTML += prom;
+}
+function getAverageSeasons(series) {
+    //Rating en este caso, por error de programación, sería igual a temporada
+    var totalCredits = 0;
+    series.forEach(function (serie) { return totalCredits = totalCredits + serie.rating; });
+    var promedio = totalCredits / series.length;
+    renderAverage(promedio);
+}
+function searchSerieByName(nameKey, series) {
+    var matchingSeries = nameKey === '' ? series : series.filter(function (serie) { return serie.name.match(nameKey); });
+    return matchingSeries[0];
+}
+//El siguiente método fue obtenido del trabajo de Diego Ortiz, las pequeñas variantes del tamaño
+//de la imagen fueron realizadas por mi al igual que la distribución de los elementos en la card
+document.addEventListener("click", function (event) {
+    var triggered = event.target;
+    if (triggered.classList.contains("clickable")) {
+        var nombre = triggered.dataset.id;
+        var serieInfo = searchSerieByName(nombre, series);
+        var cardTitle = document.querySelector('#cardTitle');
+        var cardText = document.querySelector('#cardText');
+        cardTitle.textContent = serieInfo.name;
+        cardText.innerHTML = "\n      <p><img class=\"serie-image\" src=\"".concat(serieInfo.image, "\" alt=\"").concat(serieInfo.name, " image\" width = \"250\" height =\"150\" ></p>\n      <p>").concat(serieInfo.description, "</p>\n      <p><a href=\"").concat(serieInfo.url, "\" target=\"_blank\">").concat(serieInfo.url, "</a></p>\n      \n    ");
+    }
+});
